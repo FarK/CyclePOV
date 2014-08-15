f771c8117e2255fc001bd66d742c0ea9bc084e18
Carlos Falgueras García <fark.zano@gmail.com>
2014-08-15 22:27:35 +0200

    Add TICKS_TO_MS macro.

--- CyclePOV/CyclePOV/defines.h
+++ CyclePOV/CyclePOV/defines.h
@@ -6,6 +6,7 @@
 #define DEBUG 1
 
 #define MS_TO_TICKS(ms)	(ms/portTICK_PERIOD_MS)
+#define TICKS_TO_MS(ticks) ((float)ticks*(float)portTICK_PERIOD_MS)
 
 void assert_failed(uint8_t* file, uint32_t line);
 
