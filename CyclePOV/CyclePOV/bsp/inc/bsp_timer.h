fa76deadb985cced28254a742dceea65a9f88af8
Carlos Falgueras García <fark.zano@gmail.com>
2014-09-03 01:17:32 +0200

    Created bsp for input caputure timer.

---
+++ CyclePOV/CyclePOV/bsp/inc/bsp_timer.h
@@ -0,0 +1,7 @@
+#ifndef BSP_TIMER_H_
+#define BSP_TIMER_H_
+
+void bsp_timer_init();
+float bsp_timer_getAngle();
+
+#endif /* BSP_TIMER_H_ */
---
+++ CyclePOV/CyclePOV/bsp/src/bsp_timer.c
@@ -0,0 +1,72 @@
+#include "bsp_timer.h"
+
+#include "bikeInfo.h"
+#include <tgmath.h>
+#include "stm32f4xx_tim.h"
+
+#define TIMER_FREQ 160e6
+#define TIM_TICKS_TO_US(ticks) (ticks*(10e3/TIMER_FREQ))
+
+void bsp_timer_init(){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	TIM_ICInitTypeDef TIM_ICInitStructure;
+
+	// GPIO CONFIG
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_TIM2);
+
+	// NVIC CONFIG
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x9;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	// TIMER CONFIG
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	// ADD TIMEBASE CONFIG
+	TIM_ICStructInit(&TIM_ICInitStructure);
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+
+	TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
+
+	TIM_Cmd(TIM2, ENABLE);
+}
+
+void TIM2_IRQHandler(){
+	uint32_t ticks;
+
+	if(TIM_GetITStatus(TIM2, TIM_IT_CC1) == SET){
+		ticks = TIM_GetCapture1(TIM2);
+
+		//Reconfigure timer
+		TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+		TIM_Cmd(TIM2, DISABLE);
+		TIM_SetCounter(TIM2, 0);
+		TIM_Cmd(TIM2, ENABLE);
+
+		//Refresh info
+		bikeInfo.period = TIM_TICKS_TO_US(ticks);
+		bikeInfo.velocity = 36000.0/bikeInfo.period * bikeInfo.wheelRadius * 2.0*M_PI;
+	}
+}
+
+float bsp_timer_getAngle(){
+	float time = TIM_TICKS_TO_US(TIM_GetCounter(TIM2));
+	return time/bikeInfo.period * 2.0*M_PI;
+}
