a4cec632ed6c76c2e126b588e3ae21c39796ee73
Carlos Falgueras García <fark.zano@gmail.com>
2014-09-03 00:29:43 +0200

    Adapted assert to use new bsp_leds.

--- CyclePOV/CyclePOV/assert.c
+++ CyclePOV/CyclePOV/assert.c
@@ -1,25 +1,16 @@
 #include "assert.h"
 
-#include "stm32f4_discovery.h"
+#include "bsp_leds.h"
 
 #ifdef DEBUG
 void assert_failed(uint8_t* file, uint32_t line){
-	STM_EVAL_LEDInit(LED3);
-	STM_EVAL_LEDInit(LED4);
-	STM_EVAL_LEDInit(LED5);
-	STM_EVAL_LEDInit(LED6);
-
-	STM_EVAL_LEDOff(LED3);
-	STM_EVAL_LEDOff(LED4);
-	STM_EVAL_LEDOff(LED5);
-	STM_EVAL_LEDOff(LED6);
+	bsp_leds_init();
+	bsp_leds_off();
 
 	while(1){
-		STM_EVAL_LEDToggle(LED3);
-		STM_EVAL_LEDToggle(LED4);
-		STM_EVAL_LEDToggle(LED5);
-		STM_EVAL_LEDToggle(LED6);
+		bsp_leds_toggle();
 
+		//Delay
 		int i = 1e7;
 		while(i--);
 	}
